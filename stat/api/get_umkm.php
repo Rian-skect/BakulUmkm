94cb232072ca73ef06315b24fe79714a3ef9edc8
Rian Nurdiana <136725849+Rian-skect@users.noreply.github.com>
2025-07-31 22:48:27 +0700

    Update get_umkm.php

--- stat/api/get_umkm.php
+++ stat/api/get_umkm.php
@@ -1,29 +1,8 @@
 <?php
 header("Content-Type: application/json");
 
-$supabase_url = getenv("SUPABASE_URL") . "/rest/v1/db_umkm?select=*";
-$supabase_key = trim(getenv("SUPABASE_KEY"));
-
-// Buat request
-$headers = [
-    "apikey: $supabase_key",
-    "Authorization: Bearer $supabase_key",
-    "Content-Type: application/json"
-];
-
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, $supabase_url);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-curl_setopt($ch, CURLOPT_HEADER, true); // supaya response header ikut tampil
-curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
-
-$response = curl_exec($ch);
-$http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-curl_close($ch);
-
 echo json_encode([
-    "sent_headers" => $headers,
-    "http_code" => $http_code,
-    "response" => $response
+    "SUPABASE_URL" => getenv("SUPABASE_URL"),
+    "SUPABASE_KEY_START" => substr(getenv("SUPABASE_KEY"), 0, 10),
+    "SUPABASE_KEY_LENGTH" => strlen(getenv("SUPABASE_KEY"))
 ]);
