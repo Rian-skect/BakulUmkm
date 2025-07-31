e804f471287695afd46b2d4daaa37fe2d3d4db81
Rian Nurdiana <136725849+Rian-skect@users.noreply.github.com>
2025-07-31 22:57:11 +0700

    Update get_umkm.php

--- stat/api/get_umkm.php
+++ stat/api/get_umkm.php
@@ -1,23 +1,28 @@
 <?php
 header("Content-Type: application/json");
+header("Access-Control-Allow-Origin: *");
 
 $url = "https://gykbniseplrqvrnabzdh.supabase.co/rest/v1/db_umkm?select=*";
 $key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imd5a2JuaXNlcGxycXZybmFiemRoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTMzMzEyNTcsImV4cCI6MjA2ODkwNzI1N30.0ESeTAo3RRdVkGL3UGte8-KUjBy2F8Rh40O-bo67P0w";
 
-$headers = [
-    "apikey: {$key}",
-    "Authorization: Bearer {$key}",
+$headers = implode("\r\n", [
+    "apikey: $key",
+    "Authorization: Bearer $key",
     "Content-Type: application/json"
-];
+]);
 
 $options = [
     "http" => [
         "method" => "GET",
-        "header" => implode("\r\n", $headers)
+        "header" => $headers
     ]
 ];
 
 $context = stream_context_create($options);
 $response = file_get_contents($url, false, $context);
 
-echo $response ?: json_encode(["error" => "Request gagal"]);
+if ($response === false) {
+    echo json_encode(["error" => "Request gagal"]);
+} else {
+    echo $response;
+}
